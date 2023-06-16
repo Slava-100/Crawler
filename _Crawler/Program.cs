9ad668167822edef3bba54684fcd515ccf755a9c
Svyatoslav <svat.strel.2001@gmail.com>
2023-06-16 21:37:42 +0300

    add

--- _Crawler/Program.cs
+++ _Crawler/Program.cs
@@ -8,16 +8,16 @@ using RabbitMQ;
 //await cr.ParseAsync(url);
 
 //await Task.Delay(5000);
-var getNews = Elastic.GetNewsByHead("лет");
+//var getNews = Elastic.GetNewsByHead("лет");
 
-Console.WriteLine(getNews.Hits.Count);
+//Console.WriteLine(getNews.Hits.Count);
 
-foreach (var hit in getNews.Hits)
-{
-    Console.WriteLine($"Title: {hit.Source.Title}");
-    Console.WriteLine($"Content: {hit.Source.Head}");
-    Console.WriteLine($"Date: {hit.Source.PublishDate}");
-    Console.WriteLine($"Text: {hit.Source.Text}");
-    Console.WriteLine();
-}
+//foreach (var hit in getNews.Hits)
+//{
+//    Console.WriteLine($"Title: {hit.Source.Title}");
+//    Console.WriteLine($"Content: {hit.Source.Head}");
+//    Console.WriteLine($"Date: {hit.Source.PublishDate}");
+//    Console.WriteLine($"Text: {hit.Source.Text}");
+//    Console.WriteLine();
+//}
 
